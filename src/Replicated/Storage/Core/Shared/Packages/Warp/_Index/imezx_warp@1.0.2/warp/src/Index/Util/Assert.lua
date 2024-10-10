c224d5b3766d483a4bd2c84ec10bf51722ae420d
ClintUNI <esch0007@hz.nl>
2024-10-10 17:30:20 +0200

    Work to remove Network to make branch Stable

--- src/Replicated/Storage/Core/Shared/Packages/Warp/_Index/imezx_warp@1.0.2/warp/src/Index/Util/Assert.lua
+++
@@ -1,6 +0,0 @@
---!strict
-return function(condition: (any), errorMessage: string?): ()
-	if not (condition) then
-		error(errorMessage, 2)
-	end
-end
--- src/Replicated/Storage/Core/Shared/Packages/Warp/init.lua
+++
@@ -1,2 +0,0 @@
---!strict
-return require(script._Index["imezx_warp@1.0.2"]["warp"])
