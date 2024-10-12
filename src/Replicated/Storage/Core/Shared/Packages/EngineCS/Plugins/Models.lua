e9b60033fe0c3c0d797246f822cd2b21c84cc40c
ClintUNI <esch0007@hz.nl>
2024-10-12 22:15:25 +0200

    Working Model Tracking

--- src/Replicated/Storage/Core/Shared/Packages/EngineCS/Plugins/Models.lua
+++ src/Replicated/Storage/Core/Shared/Packages/EngineCS/Plugins/Models.lua
@@ -16,34 +16,22 @@ local ModelComponent: Types.ComponentWithType<Instance> = Components.new("Model"
 local ModelTrackingQueue: number = MessageBus.new("ModelTracking")
 local ModelDestroyingQueue: number = MessageBus.new("ModelDestroying")
 
-local function createModelForEntity(entity, model, cleaningEvent: RBXScriptSignal?)
-    Entities:give(entity, {[ModelComponent] = model})
-    if cleaningEvent then
-        print('here')
-        cleaningEvent:Once(function(): ()
+local function createModelForEntity(entity, model)
+    model.Destroying:Once(
+        function(): ()
             MessageBus.queue(ModelDestroyingQueue, {Entity = entity, Model = model});
-        end)
-    else
-        model.Destroying:Once(
-            function(): ()
-                MessageBus.queue(ModelDestroyingQueue, {Entity = entity, Model = model});
-            end
-        )
-    end
+        end
+    )
 end
 
 Systems:on_update(system, function(world: Types.World): ()
-    for _, modelData: { Model: Instance, Entity: Types.Entity, ModelCreationEvent: RBXScriptSignal?, CleaningEvent: RBXScriptSignal? } in MessageBus.read(ModelDestroyingQueue) do
+    for _, modelData: { Model: Instance, Entity: Types.Entity } in MessageBus.read(ModelDestroyingQueue) do
         Entities:rid(modelData.Entity, ModelComponent);
     end
     -- MessageBus.consume(ModelDestroyingQueue)
-    for _, modelData: { Model: Instance, Entity: Types.Entity, ModelCreationEvent: RBXScriptSignal?, CleaningEvent: RBXScriptSignal? } in MessageBus.read(ModelTrackingQueue) do
+    for _, modelData: { Model: Instance, Entity: Types.Entity } in MessageBus.read(ModelTrackingQueue) do
         if modelData.Model then
-            createModelForEntity(modelData.Entity, modelData.Model, modelData.CleaningEvent)
-        elseif modelData.ModelCreationEvent then
-            modelData.ModelCreationEvent:Once(function(model)
-                createModelForEntity(modelData.Entity, model, modelData.CleaningEvent)
-            end)
+            createModelForEntity(modelData.Entity, modelData.Model)
         end
     end
     MessageBus.consume(ModelTrackingQueue)
